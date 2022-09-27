81a2d023e55741e6ef2ff62d45239f6502781030
muksis <60219890+muksis@users.noreply.github.com>
2022-09-26 23:14:53 -0400

    Add header and calculator to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Header from './components/Header';
+import Calculator from './components/Calculator';
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className='app'>
+      <Header />
+      <Calculator />
+    </div>
+  );
 }
 
 export default App;
