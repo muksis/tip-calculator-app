861ce30c25601795a11829ee0c59f176765c1d02
muksis <60219890+muksis@users.noreply.github.com>
2022-09-26 23:58:48 -0400

    Add calculator component outline

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import { Container, Row, Col, Form, InputGroup } from 'react-bootstrap';
+import iconDollar from '../images/icon-dollar.svg';
+import iconPerson from '../images/icon-person.svg';
 
 const Calculator = () => {
-  return <div>Calculator</div>;
+  return (
+    <Container className='calculator'>
+      <Row>
+        <Col>
+          <Form.Label htmlFor='inputBill'>Bill</Form.Label>
+          {/* Bill */}
+          <InputGroup>
+            <InputGroup.Text>
+              <img src={iconDollar} alt='Dollar icon' />
+            </InputGroup.Text>
+            <Form.Control id='inputBill' placeholder='0' autoComplete='off' />
+          </InputGroup>
+          {/* Tip */}
+          <Form.Label>Select Tip %</Form.Label>
+          {/* Tip Selector */}
+          {/* Number of People */}
+          <Form.Label htmlFor='inputNumberOfPeople'>
+            Number of People
+          </Form.Label>
+          <InputGroup>
+            <InputGroup.Text>
+              <img src={iconPerson} alt='Person icon' />
+            </InputGroup.Text>
+            <Form.Control id='inputBill' placeholder='0' autoComplete='off' />
+          </InputGroup>
+        </Col>
+        <Col>{/* Results */}</Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Calculator;
